682194bb6001a9a12920afdb4257657dc4157d31
shamspias <shamsuddinpias0@gmail.com>
2023-04-14 14:49:01 +0600

    fix: voice format

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ def incoming_sms():
         sound.export(mp4_file, format="mp4")
 
         resp = MessagingResponse()
-        resp.message(new_response_text)
+        resp.message(body=new_response_text, media=mp4_file)
         # resp.message(new_response_text).media(mp4_file)
 
         # Delete the temporary files
